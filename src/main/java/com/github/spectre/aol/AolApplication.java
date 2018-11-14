fed02b263cc18790214d62a672b3ca4069e80fe2
wanyang19880625 <296866643@qq.com>
2018-11-15 00:20:44 +0800

    init

---
+++ src/main/java/com/github/spectre/aol/AolApplication.java
@@ -0,0 +1,12 @@
+package com.github.spectre.aol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AolApplication.class, args);
+	}
+}
